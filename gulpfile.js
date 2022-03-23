cdf7c8fe5f9580d643eb3210834397b14eaee3dd
storrellas <storrellas@gmail.com>
2022-03-23 09:18:14 +0100

    ENH: Added gulp to equation

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,19 @@
 var gulp = require('gulp');
 var gls = require('gulp-live-server');
+var browserSync = require('browser-sync').create();
 gulp.task('serve', function() {
   //1. serve with default settings
   var server = gls.static('public', 8080); //equals to gls.static('public', 3000);
   server.start();
 })
+
+gulp.task('browser-sync', function() {
+  browserSync.init({    
+      server: { baseDir: "./public" },
+      port: 8080
+  });
+
+  // Watch
+  gulp.watch(['public/*']).on("change", () => browserSync.reload() );
+
+});
