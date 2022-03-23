8e07c9eb21b50b14bde2caded406cfcfeeb1e523
storrellas <storrellas@gmail.com>
2022-03-23 11:40:09 +0100

    ENH: Changed React component

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,10 @@
 var gulp = require('gulp');
-var gls = require('gulp-live-server');
 var browserSync = require('browser-sync').create();
-gulp.task('serve', function() {
-  //1. serve with default settings
-  var server = gls.static('public', 8080); //equals to gls.static('public', 3000);
-  server.start();
-})
 
 gulp.task('browser-sync', function() {
   browserSync.init({    
       server: { baseDir: "./nc2ui" },
-      port: 8080
+      port: 3000
   });
 
   // Watch
